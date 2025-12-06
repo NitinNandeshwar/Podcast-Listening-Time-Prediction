d615f25400139cfa6bd544f3839d5e35864fb6ff
NitinNandeshwar <samarthtnitin64@gmail.com>
2025-12-06 21:37:14 +0000

    Project Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The dataset contains information about various podcast episodes and their attributes. The goal is to analyze and predict the average listening duration of podcast episodes based on various features',
+    author='Nitin Nandeshwar',
+    license='MIT',
+)
